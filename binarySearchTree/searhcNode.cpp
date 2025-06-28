7a049288657e57ef3b4e57362db3f78194813c85
hritik12345444 <hritik12345444@gmail.com>
2025-06-28 09:30:44 +0530

    search a node in binary search tree

--- binarySearchTree/searhcNode.cpp
+++ binarySearchTree/searhcNode.cpp
@@ -1,5 +1,4 @@
 // Search a node in BST return true if it is present else return false
-
 #include<iostream>
 #include<vector>
 using namespace std;
@@ -16,9 +15,105 @@ public:
     }
 };
 
+// insert element in bst
 Node* insertInBST(Node* root, int val){
     if(root == nullptr){
-        return new Node(val);
+        Node* new_node = new Node(val);
+        root = new_node;
+        return root;
     }
+
+    if(val > root->data){
+       root->right = insertInBST(root->right,val);
+    }else{
+        root->left = insertInBST(root->left,val);
+    }
+
+    return root;
+}
+
+// Build a BST
+Node* buildBST(vector<int> sequence){
+    Node* root = nullptr;
+
+    for(int i= 0; i<sequence.size(); i++){
+        root = insertInBST(root,sequence[i]);
+    }
+
+    return root;
+}
+
+// inOrder printing
+void inOrder(Node* root){
+    if(root == nullptr){
+        return;
+    }
+
+    inOrder(root->left);
+    cout << root->data << " ";
+    inOrder(root->right);
     
+    return;
+}
+
+// preOrder
+void preOrder(Node* root){
+    if(root == nullptr){
+        return;
+    }
+
+    cout << root->data << " ";
+    preOrder(root->left);
+    preOrder(root->right);
+
+    return;
+}
+
+// postOrder
+void postOrde(Node* root){
+    if(root == nullptr){
+        return;
+    }
+
+    postOrde(root->left);
+    postOrde(root->right);
+    cout << root->data << " ";
+    return ;
+}
+
+
+// search a Node in BST
+bool searchInBST(Node* root,int val){
+    if(root == nullptr){
+        return false;
+    }
+    if(root->data == val){
+        return true;
+    }
+
+    if(val > root->data){
+        return searchInBST(root->right,val);
+    }else{
+        return searchInBST(root->left,val);
+    }
+
+    return false;
+}
+
+
+int main(){
+
+    vector<int> sequence={3,2,1,5,6,4};
+
+    Node* root = buildBST(sequence);
+
+    inOrder(root);
+    cout << endl;
+    preOrder(root);
+    cout << endl;
+    postOrde(root);
+    cout << endl;
+
+    cout << searchInBST(root,6);
+    return 0;
 }
